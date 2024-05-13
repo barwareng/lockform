fe7214d14937970e81644ea4aee07f0f32c91a07
idsmoura <cleophasbarwareng@ids.company>
2024-05-13 04:25:36 +0300

    feat: Initial Landing UI

---
+++ landing/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import '../app.css';
+	// import { ModeWatcher } from 'mode-watcher';
+	// import { Toaster } from '$lib/components/ui/sonner';
+	// import { MetaTags } from 'svelte-meta-tags';
+	// import { VITE_APP_NAME } from '$lib/env';
+</script>
+
+<!-- <MetaTags title={VITE_APP_NAME} /> -->
+<!-- <ModeWatcher /> -->
+<!-- <Toaster /> -->
+
+<slot />

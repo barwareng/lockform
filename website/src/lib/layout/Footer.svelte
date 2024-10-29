e3d36c00c71a73eb11aa044e2cf51fc9aae1833e
idsmoura <cleophas.barwareng@gmail.com>
2024-10-29 08:07:43 +0300

    footer

--- website/src/lib/layout/Footer.svelte
+++ website/src/lib/layout/Footer.svelte
@@ -1,53 +1,62 @@
 <script>
+	import { page } from '$app/stores';
+	import { PUBLIC_PLATFORM_BASE_URL } from '$env/static/public';
+	import LockformLogo from '$lib/icons/LockformLogo.svelte';
 	import DiscordSvg from '$lib/imgs/discord.svg';
 	import TwitterSvg from '$lib/imgs/x.svg';
+	import { Target } from 'lucide-svelte';
 
 	const footerNavs = [
-		{
-			label: 'Product',
-			items: [
-				{
-					href: '/',
-					name: 'Email Collection'
-				},
-				{
-					href: '/pricing',
-					name: 'Pricing'
-				},
-				{
-					href: '/faq',
-					name: 'FAQ'
-				}
-			]
-		},
+		// {
+		// 	label: 'Product',
+		// 	items: [
+		// 		{
+		// 			href: '/',
+		// 			name: 'Email Collection'
+		// 		},
+		// 		{
+		// 			href: '/pricing',
+		// 			name: 'Pricing'
+		// 		},
+		// 		{
+		// 			href: '/faq',
+		// 			name: 'FAQ'
+		// 		}
+		// 	]
+		// },
 
 		{
 			label: 'Community',
 			items: [
 				{
-					href: '/',
+					href: 'https://discord.com/channels/930471500480413746/1293654482801922130',
+					target: '_blank',
 					name: 'Discord'
-				},
-				{
-					href: '/',
-					name: 'Twitter'
-				},
-				{
-					href: 'mailto:hello@chatcollect.com',
-					name: 'Email'
 				}
+				// {
+				// 	href: '/',
+				// 	name: 'Twitter'
+				// },
+				// {
+				// 	href: 'mailto:hello@chatcollect.com',
+				// 	name: 'Email'
+				// }
 			]
 		},
 		{
 			label: 'Legal',
 			items: [
 				{
-					href: '/terms',
+					href: `${$page.url.origin}/pdf/LockformToS.pdf`,
+					target: '_blank',
+					download: 'LockformToS.pdf',
 					name: 'Terms'
 				},
 
 				{
-					href: '/privacy',
+					href: `${$page.url.origin}/pdf/LockformPrivacy.pdf`,
+					target: '_blank',
+					download: 'LockformPrivacy.pdf',
 					name: 'Privacy'
 				}
 			]
@@ -56,15 +65,16 @@
 
 	const footerSocials = [
 		{
-			href: '',
+			href: 'https://discord.com/channels/930471500480413746/1293654482801922130',
+			target: '_blank',
 			name: 'Discord',
 			icon: DiscordSvg
-		},
-		{
-			href: '',
-			name: 'Twitter',
-			icon: TwitterSvg
 		}
+		// {
+		// 	href: '',
+		// 	name: 'Twitter',
+		// 	icon: TwitterSvg
+		// }
 	];
 </script>
 
@@ -72,27 +82,11 @@
 	<div class="mx-auto w-full max-w-screen-xl xl:pb-2">
 		<div class="gap-4 p-4 px-8 py-16 sm:pb-16 md:flex md:justify-between">
 			<div class="mb-12 flex flex-col gap-4">
-				<a href="https://animation-svelte.vercel.app" class="flex items-center gap-2">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="24"
-						height="24"
-						viewBox="0 0 24 24"
-						fill="none"
-						stroke="currentColor"
-						stroke-width="1"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						class="lucide lucide-floor-plan size-8"
-						><path
-							d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-5"
-						/><path d="M9 3v7" /><path d="M21 10h-7" /><path d="M3 15h9" /></svg
-					>
-					<span class="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
-						Svee UI
-					</span>
+				<a class="flex items-center gap-x-2 text-lg font-black" href="/">
+					<LockformLogo class="h-6 w-6" />
+					lockform
 				</a>
-				<p class="max-w-xs">UI Library for Design Engineers</p>
+				<p class="max-w-xs">Protection for your team</p>
 			</div>
 			<div class="grid grid-cols-1 gap-8 sm:grid-cols-3 sm:gap-10">
 				{#each footerNavs as nav}
@@ -107,6 +101,8 @@
 								<li>
 									<a
 										href={item.href}
+										target={item.target}
+										download={item.download}
 										class="cursor-pointer text-sm font-[450] text-gray-400 duration-200 hover:text-gray-200"
 									>
 										{item.name}
@@ -126,6 +122,7 @@
 				{#each footerSocials as social}
 					<a
 						href={social.href}
+						target={social.target}
 						class="fill-gray-500 text-gray-500 hover:fill-gray-900 hover:text-gray-900 dark:hover:fill-gray-600 dark:hover:text-gray-600"
 					>
 						<img src={social.icon} class="size-4" alt={social.name} />
@@ -133,12 +130,12 @@
 					</a>
 				{/each}
 			</div>
-			<span class="text-sm text-gray-500 dark:text-gray-400 sm:text-center">
+			<span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
 				Copyright ©
 				{' '}
 				{new Date().getFullYear()}
 				{' '}
-				<a href="/" class="cursor-pointer">Svee UI</a>
+				<a href="/" class="cursor-pointer">Lockform</a>
 				. All Rights Reserved.
 			</span>
 		</div>
